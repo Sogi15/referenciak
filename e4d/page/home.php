338e4c6fc80c3d75d4d1f8b8596f697a2b150c52
DémonMesteR <90307491+Sogi15@users.noreply.github.com>
2023-06-19 15:48:43 +0200

    Angol nyelvtanulási projekt weblap (2023-05)
    English 4 Dummies weblap ötlet végül elkaszálásra került. A tesztek oldalon játékok kerültek létrehozásra ez a weblap fő eleme.

---
+++ e4d/page/home.php
@@ -0,0 +1,6 @@
+<p class="pagetitle">Főoldal</p>
+<?php echo '<h1 class="text-center">Üdvözöllek: <i class="home-name">'.$_SESSION['name'].'</i></h1>'; ?>
+<p class="tx-justify">
+Célünk az volt,hogy egy olyan oktatóoldalt létrehozni, ami segít az angol nyelv tanulásában, személyre szabott gyakorlatokkal (nyelvtan, szókincs, írás készség, hallás utáni értés).
+Valamint lehetővé teszi gyakorló feladatok végrehajtását. Egyenlőre csak prototípus lenne ez az oldal. Közösen dolgoztunk rajta XY-al. Végül a projekt elkaszálásra került 2023.05.18-án
+</p>
